a1bc12d760db8cdd6e6b8a45701a13c50da56f39
runqingz <runqing.z@gmail.com>
2017-04-01 01:56:40 -0700

    fix parse py

--- App/parse.py
+++ App/parse.py
@@ -3,15 +3,12 @@ from .product import *
 
 def parse(obj):
 
-    product_detail = ProductDetail('iPhone7')
+    product_detail = ProductDetail(obj['name'])
 
-    product_detail.add_spec({'displayName': 'Color', 'Value': 'Red'})
-    product_detail.add_spec({'displayName': 'Capacity', 'Value': '132 GB'})
+    for item in obj['specifications']:
+        spec = Specification(item['name'], item['value'])
+        product_detail.add_spec(spec)
 
-    #for item in obj['spces']:
-    #    spec = Specification(item['name'], item['value'])
-    #    product_detail.add_spec(spec)
-    #
     product = add_one(product_detail.to_dict())
 
     return product
