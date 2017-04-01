f15a08d6e2d23121af1cc22054908e49c8a7d39f
runqingz <runqing.z@gmail.com>
2017-04-01 01:15:40 -0700

    mod parse py
    Mod to add hardcoded

--- App/parse.py
+++ App/parse.py
@@ -3,12 +3,16 @@ from .product import *
 
 def parse(obj):
 
-    product_detail = ProductDetail(obj['name'])
+    product_detail = ProductDetail('iPhone7')
 
-    for item in obj['spces']:
-        spec = Specification(item['name'], item['value'])
-        product_detail.add_spec(spec)
+    product_detail.add_spec({'displayName': 'Color', 'Value': 'Red'})
+    product_detail.add_spec({'displayName': 'Capacity', 'Value': '132 GB'})
 
+    #for item in obj['spces']:
+    #    spec = Specification(item['name'], item['value'])
+    #    product_detail.add_spec(spec)
+    #
     product = add_one(product_detail.to_dict())
 
     return product
+
