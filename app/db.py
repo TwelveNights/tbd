a07b04cce6fb8015f5f190f294fdad8920325b7f
runqingz <runqing.z@gmail.com>
2017-04-01 07:23:02 -0700

    add find_instance to deal with objectId
    transfer objectId to string and compare with input string

--- app/db.py
+++ app/db.py
@@ -1,34 +1,63 @@
 import pymongo as pm
-from pymongo import ReturnDocument
+from pymongo import *
 
 client = pm.MongoClient()
 db = client['product_db']
 products = db['products']
 
+
 def add_one(request):
+
     result = products.insert_one(request)
     item_id = result.inserted_id
     product = products.find_one({'_id': item_id})
-    product['_id'] = str(product['_id'])
+    product['_id'] = product['_id'].toString()
     return product
+
 
 def get_one(id):
-    product = products.find_one({'_id': id})
+    product = find_instance(id)
     return product
 
+
 def remove_one(id):
-    deletedItem = products.find_one_and_delete({'_id': id})
-    return deletedItem
+    item = find_instance(id)
+
+    if item is not None:
+        objid = item['_id']
+        deleted_item = products.find_one_and_delete({'_id': objid})
+        return deleted_item
+
+    return None
+
 
 def remove_all(request):
     products.delete_many(request)
 
+
 def add_all(products):
     products.insert_many(products)
+
 
 def get_all(request):
     cursor = products.find(request)
     return cursor
 
+
 def update(id, request):
-    return products.find_one_and_replace({'_id': id}, request, return_document=ReturnDocument.AFTER)
+    item = find_instance(id)
+
+    if item is not None:
+        objid = item['_id']
+        return products.find_one_and_replace({'_id': objid}, request, return_document=ReturnDocument.AFTER)
+
+    return None
+
+def find_instance(id):
+    cursor = products.find({})
+
+    for item in cursor:
+        if str(item['_id']) == str(id):
+            return item
+    return None
+
