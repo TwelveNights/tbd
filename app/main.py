8c645a31616c74117cf013b3ebbbc7457312fe00
Stephanie Chan <stephaniechan219@gmail.com>
2017-03-31 23:33:55 -0700

    incomplete parse in flask

--- app/main.py
+++ app/main.py
@@ -1,11 +1,31 @@
-from flask.ext.api import FlaskAPI
+from flask.ext.api import FlaskAPI, exceptions
+from flask import request
 
 app = FlaskAPI(__name__)
 
-@app.route('/')
-def hello_world():
-    return { 'message': 'Hello World!' }
 
+@app.route('/parse/', methods=['GET', 'POST'])
+def parse():
+    data = request.data
+    if "urls" not in data:
+        raise exceptions.APIException("Bad request")
+
+    # for entry, i in enumerate(request.data):
+    #    if (request.data[i] != None):
+    #         raise exceptions.NotFound()
+    #    else:
+    #         pass1
+
+    return {
+        'message': str("Hello")
+    }
+
+
+"""
+{ "urls" : ["cs.ubc.ca", "google.com"] }
+{"urls" : []}
+
+"""
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
